8dd303d549e3595d12bdcb0c4abef254e16b2f40
Josue-rg <20233tn200@utez.edu.mx>
2025-05-09 01:46:41 -0600

    Endpoint: Select active word, finished

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,20 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule } from '@nestjs/config';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { User } from '../entities/user.entity';
 
 @Module({
   imports: [
+    ConfigModule,
     TypeOrmModule.forFeature([User]),
+    PassportModule,
     JwtModule.register({
       secret: 'clave',
       signOptions: { expiresIn: '1h' },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
